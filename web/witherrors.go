c4efe08161e32194d72009674fc9732d365e5504
tonythetender <elt.97@protonmail.com>
2025-09-04 03:59:34 -0400

    Add WebConnWithErrors wrapper for enhanced error handling
    Introduce `WebConnWithErrors` to wrap `WebConn` and streamline error propagation for web operations.

---
+++ web/witherrors.go
@@ -0,0 +1,47 @@
+package web
+
+import (
+	"log"
+	"net/url"
+
+	"github.com/tonythetender/pwngears"
+)
+
+type WebConnWithErrors struct {
+	Conn *WebConn
+}
+
+func ConnWithErrors(baseUrl string) (*WebConnWithErrors, error) {
+	logger, err := pwngears.NewDefaultLogger("IGNORE")
+	if err != nil {
+		log.Fatal("error generating the default logger: %v", err)
+	}
+	conn, err := ConnCore(logger, baseUrl)
+	if err != nil {
+		return nil, err
+	}
+	connWithErrors := WebConnWithErrors{
+		Conn: conn,
+	}
+	return &connWithErrors, nil
+}
+
+func (c *WebConnWithErrors) Get(path string, opts ...RequestOption) (*Response, error) {
+	return c.Conn.Get(path, opts...)
+}
+
+func (c *WebConnWithErrors) Post(path string, data url.Values, opts ...RequestOption) (*Response, error) {
+	return c.Conn.Post(path, data, opts...)
+}
+
+func (c *WebConnWithErrors) DisableRedirect() {
+	c.Conn.DisableRedirect()
+}
+
+func (c *WebConnWithErrors) SetCookie(name, value string) error {
+	err := c.Conn.SetCookie(name, value)
+	if err != nil {
+		return err
+	}
+	return nil
+}
